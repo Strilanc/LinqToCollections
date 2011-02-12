188bc653b4b8a0f0dd47aba01888beb8c5fb78ef
unknown <Craig@.(none)>
2011-02-12 15:48:56 -0400

    Started RistExtensions class - Added AsRist/ToRist methods - Unit tested new methods

---
+++ LinqToLists/RistExtensions.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqToLists {
+    public static class RistExtensions {
+        ///<summary>Exposes a list as a readable list.</summary>
+        [Pure()]
+        public static IRist<T> AsRist<T>(this IList<T> list) {
+            Contract.Requires(list != null);
+            Contract.Ensures(Contract.Result<IRist<T>>() != null);
+            Contract.Ensures(Contract.Result<IRist<T>>().Count == list.Count);
+            Contract.Ensures(Contract.Result<IRist<T>>().SequenceEqual(list));
+            var r =  new Rist<T>(getter: i => list[i],
+                                 counter: () => list.Count,
+                                 efficientIterator: list);
+            Contract.Assume(r.Count == list.Count);
+            Contract.Assume(r.SequenceEqual(list));
+            return r;
+        }
+        ///<summary>Creates a copy of the given sequence and exposes the copy as a readable list.</summary>
+        [Pure()]
+        public static IRist<T> ToRist<T>(this IEnumerable<T> sequence) {
+            Contract.Requires(sequence != null);
+            Contract.Ensures(Contract.Result<IRist<T>>() != null);
+            Contract.Ensures(Contract.Result<IRist<T>>().SequenceEqual(sequence));
+            var r = sequence.ToArray().AsRist();
+            Contract.Assume(r.SequenceEqual(sequence));
+            return r;
+        }
+        ///<summary>Exposes the underlying list of a given sequence as a readable list, creating a copy if the underlying type is not a list.</summary>
+        ///<remarks>Just a cast when the sequence is an IRist, and equivalent to AsRist(IList) when the sequence is an IList.</remarks>
+        [Pure()]
+        public static IRist<T> AsRist<T>(this IEnumerable<T> sequence) {
+            Contract.Requires(sequence != null);
+            Contract.Ensures(Contract.Result<IRist<T>>() != null);
+            Contract.Ensures(Contract.Result<IRist<T>>().SequenceEqual(sequence));
+            
+            var asRist = sequence as IRist<T>;
+            if (asRist != null) {
+                Contract.Assume(asRist.SequenceEqual(sequence));
+                return asRist;
+            }
+            
+            var asList = sequence as IList<T>;
+            if (asList != null) {
+                var r = asList.AsRist();
+                Contract.Assume(r.SequenceEqual(sequence));
+                return r;
+            }
+            
+            return sequence.ToRist();
+        }
+    }
+}
---
+++ LinqToListsTest/RistExtensionsTest.cs
@@ -0,0 +1,45 @@
+﻿using LinqToLists;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace LinqToListsTest {
+    [TestClass()]
+    public class RistExtensionsTest {
+        [TestMethod()]
+        public void AsRistIListTest() {
+            Assert.IsTrue(new[] { 0, 1, 2 }.AsRist().Count == 3);
+            Assert.IsTrue(new[] { 0, 2, 5, 7 }.AsRist().SequenceEqual(new[] { 0, 2, 5, 7 }));
+            var list = new List<int>() { 2, 3 };
+            var rist = list.AsRist();
+            Assert.IsTrue(rist.SequenceEqual(new[] { 2, 3 }));
+            list.Add(5);
+            Assert.IsTrue(rist.SequenceEqual(new[] { 2, 3, 5 }));
+        }
+        [TestMethod()]
+        public void AsRistIEnumerableTest() {
+            var list = new List<int>() { 2, 3 };
+            var listAsRist = list.AsEnumerable().AsRist();
+            var projectionAsRist = list.Select(i => i + 1).AsRist();
+            
+            //IRist ==> gives back input
+            Assert.ReferenceEquals(listAsRist, listAsRist.AsEnumerable().AsRist());
+
+            //IList ==> viewed as Rist, IEnumerable ==> copied
+            Assert.IsTrue(listAsRist.SequenceEqual(new[] { 2, 3 }));
+            Assert.IsTrue(projectionAsRist.SequenceEqual(new[] { 3, 4 }));
+            list.RemoveAt(1);
+            Assert.IsTrue(listAsRist.SequenceEqual(new[] { 2 }));
+            Assert.IsTrue(projectionAsRist.SequenceEqual(new[] { 3, 4 }));
+        }
+        [TestMethod()]
+        public void ToRistTest() {
+            var list = new List<int>() { 2, 3 };
+            var rist = list.ToRist();
+            Assert.IsTrue(rist.SequenceEqual(new[] { 2, 3 }));
+            list.Add(1);
+            Assert.IsTrue(rist.SequenceEqual(new[] { 2, 3 }));
+        }
+    }
+}
