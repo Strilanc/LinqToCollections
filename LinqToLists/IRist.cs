3294e7c28a034c4481feae68341d4ab8495c31ee
unknown <Craig@.(none)>
2011-02-11 23:56:39 -0400

    Defined the IRist (readable list) interface

---
+++ LinqToLists/IRist.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Diagnostics.CodeAnalysis;
+
+namespace LinqToLists {
+    ///<summary>A readable list of items with known length.</summary>
+    ///<remarks>Shorthand for 'IReadableList'.</remarks>
+    ///<typeparam name="T">The type of items in the list.</typeparam>
+    [ContractClass(typeof(IRistContractClass<>))]
+    public interface IRist<T> : IEnumerable<T> {
+        ///<summary>Gets the number of items in the list.</summary>
+        int Count { get; }
+        ///<summary>Gets the list item at the given index.</summary>
+        T this[int index] { get; }
+    }
+
+    [ContractClassFor(typeof(IRist<>))]
+    public abstract class IRistContractClass<T> : IRist<T> {
+        public int Count {
+            get {
+                Contract.Ensures(Contract.Result<int>() >= 0);
+                throw new NotImplementedException();
+            }
+        }
+        public T this[int index] {
+            get {
+                Contract.Requires<ArgumentException>(index >= 0);
+                Contract.Requires<ArgumentException>(index < this.Count);
+                throw new NotImplementedException();
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator() {
+            throw new NotImplementedException();
+        }
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() {
+            throw new NotImplementedException();
+        }
+    }
+}
